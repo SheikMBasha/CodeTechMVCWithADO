fb02a74c49ca64526e325152cb239bb1d20514e3
SheikMBasha <basha.mjcet@gmail.com>
2014-02-04 00:48:24 +0530

    Trying to Add WCF Service to MVC

---
+++ CodeTechnologiesMVCADO/Controllers/AcademyController.cs
@@ -0,0 +1,47 @@
+﻿using CodeTechnologiesMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodeTechnologiesMVCADO.Controllers
+{
+    public class AcademyController : Controller
+    {
+        public ActionResult Index()
+        {
+             
+            //List<AcademyModel> academyList = CodeTechnologiesMVCADO.
+            return View();
+        }
+
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        //[HttpPost]
+        //public ActionResult Create(academy academyObj)
+        //{
+        //}
+
+        //public ActionResult Details(int id)
+        //{
+        //}
+
+        //public ActionResult Edit(int id)
+        //{
+        //}
+
+        //[HttpPost]
+        //public ActionResult Edit(academy academyObj)
+        //{
+        //}
+
+        //public ActionResult Delete(int id)
+        //{
+        //}
+
+    }
+}
