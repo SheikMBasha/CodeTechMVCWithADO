3fd6b3c6af70fc7161f86e3d2452ed1666b7f665
SheikMBasha <basha.mjcet@gmail.com>
2014-02-04 20:38:35 +0530

    Fixed bug with GetAcademy

--- CodeTechnologiesMVCADO/Controllers/AcademyController.cs
+++ CodeTechnologiesMVCADO/Controllers/AcademyController.cs
@@ -1,47 +1,58 @@
-﻿using CodeTechnologiesMVC.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CodeTechnologiesMVCADO.CodeTechWCF;
 
 namespace CodeTechnologiesMVCADO.Controllers
 {
     public class AcademyController : Controller
     {
+        CodeTechnologiesServicesClient proxy = new CodeTechnologiesServicesClient();
         public ActionResult Index()
         {
-             
-            //List<AcademyModel> academyList = CodeTechnologiesMVCADO.
+
+            var academyList = proxy.GetAllAcademies();
+            return View(academyList);
+        }
+
+        public ActionResult Create()
+        {
             return View();
         }
 
-        //public ActionResult Create()
-        //{
-        //    return View();
-        //}
+        [HttpPost]
+        public ActionResult Create(AcademyModel academyObj)
+        {
+            proxy.AdddAcademy(academyObj);
+            return RedirectToAction("Index");
+        }
 
-        //[HttpPost]
-        //public ActionResult Create(academy academyObj)
-        //{
-        //}
+        public ActionResult Details(int id)
+        {
+            var academyObj = proxy.getAcademy(id);
+            return View(academyObj);
+        }
 
-        //public ActionResult Details(int id)
-        //{
-        //}
+        public ActionResult Edit(int id)
+        {
+            var academyObj = proxy.getAcademy(id);
+            return View(academyObj);
+        }
 
-        //public ActionResult Edit(int id)
-        //{
-        //}
+        [HttpPost]
+        public ActionResult Edit(AcademyModel academyObj)
+        {
+            proxy.UpdateAcademy(academyObj);
+            return RedirectToAction("Index");
+        }
 
-        //[HttpPost]
-        //public ActionResult Edit(academy academyObj)
-        //{
-        //}
-
-        //public ActionResult Delete(int id)
-        //{
-        //}
+        public ActionResult Delete(int id)
+        {
+            proxy.DeleteAcademy(id);
+            return RedirectToAction("Index");
+        }
 
     }
 }
